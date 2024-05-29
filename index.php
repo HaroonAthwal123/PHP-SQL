2e5320bdb8b6cee5b228a5e14eee02e643fe40b5
Haroon waris <162343814+HaroonAthwal123@users.noreply.github.com>
2024-05-29 20:29:41 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+//for ($i = 1; $i < 101; $i++) {
+  //    if ($i == 5) {
+    //      continue;
+      //}
+      //echo ($i ." <br>");
+  //}
+
+
+
+
+  //table
+  //$num = 10;
+  //for ($i=1;$i<= 10; $i++)
+  //{
+   // echo $num . "x". "$i"."=".$num*$i;
+    //echo"</br>";
+  //}
+ 
+
+//$number = 222;
+// yaha par apna number daalain
+
+// check if the number is odd or even
+//if ($number % 2 ==0) {
+  //  echo"$number is an even number.";
+    
+//}else {
+  //  echo"$number is an odd number.";
+//}
+
+
+
+
+?>
+
+                          
+
+
+
+
+ 
+ 
+ 
+
+
+?>
